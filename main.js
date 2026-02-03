82e78b4e1bba77f949ea04a90216d1c490d9378f
dalmazan01 <143827065+dalmazan01@users.noreply.github.com>
2026-02-03 18:17:52 +0000

    Change background color

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ function display() {
     canvas.attr('height', canvas.height());
     var c = canvas[0].getContext('2d');
     c.fillStyle = 'rgb(14, 46, 78)';  // Your chosen color
-
+    c.fillRect(0, 0, canvas.width(), canvas.height());
     if (!(info & 1)) {
         c.fillStyle = colorString(0, 0, 0);
         c.font = '10pt Helvetica';
