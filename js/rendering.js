706e553ad5dc43bc746e59dd062c3882d5542bf6
Angel Almaraz <almaae0616@gmail.comgit statusgit config --global user.name Angel>
2026-04-30 09:41:46 -0500

    a

--- js/rendering.js
+++ js/rendering.js
@@ -270,6 +270,53 @@ function display() {
         }
     });
 
+    // Highlight complementary nodes (open and closed nodes of the same name) with yellow halo
+    // Only when a node is selected
+    if (showLabels && curVertex !== undefined) {
+        // Get the name and style of the selected node
+        var selectedName = nodeNames[curVertex] || String.fromCharCode(65 + curVertex);
+        var selectedStyle = (curVertex in openNodes) ? (openNodes[curVertex] ? 'open' : 'filled') : nodeStyle;
+        
+        // Find complementary nodes (same name, different style)
+        var complementaryNodes = [];
+        _.each(link.vertices, function(v, i) {
+            if (i !== curVertex && (i == curVertex || !(view & 2))) {
+                var baseName = nodeNames[i] || String.fromCharCode(65 + i);
+                var thisNodeStyle = (i in openNodes) ? (openNodes[i] ? 'open' : 'filled') : nodeStyle;
+                
+                if (baseName === selectedName && thisNodeStyle !== selectedStyle) {
+                    complementaryNodes.push({index: i, style: thisNodeStyle, pos: v});
+                }
+            }
+        });
+        
+        // Highlight each complementary node
+        _.each(complementaryNodes, function(n) {
+            // Check if this complementary node is connected to another complementary node
+            var isConnectedToComplementary = _.some(link.edges, function(e) {
+                if (e.i === n.index || e.j === n.index) {
+                    var otherIndex = (e.i === n.index) ? e.j : e.i;
+                    var otherName = nodeNames[otherIndex] || String.fromCharCode(65 + otherIndex);
+                    return otherName === selectedName;
+                }
+                return false;
+            });
+            
+            // Use green halo if connected to complementary node, yellow otherwise
+            var haloColor = isConnectedToComplementary ? 'rgba(0, 255, 0, 0.8)' : 'rgba(255, 255, 0, 0.8)';
+            
+            c.save();
+            c.shadowBlur = 15;
+            c.shadowColor = haloColor;
+            c.strokeStyle = haloColor;
+            c.lineWidth = 3;
+            c.beginPath();
+            c.arc(n.pos[0], n.pos[1], VERTEX_SIZE + 5, 0, 2 * Math.PI);
+            c.stroke();
+            c.restore();
+        });
+    }
+
     if (attractor) {
         c.fillStyle = colorString(0.5, 0.5, 0.5)
         fillPoint(c, attractor);
